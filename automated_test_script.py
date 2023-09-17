8e2ccb6f759b46c8a716475ed91250b800317f98
poleonia <143344116+poleonia@users.noreply.github.com>
2023-09-17 12:48:53 -0500

    Add files via upload

--- automated_test_script.py
+++ automated_test_script.py
@@ -1,47 +1,110 @@
-
 import json
+
 
 def load_problems(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
+
 def generate_solution(problem):
     # This is a placeholder. In a real-world scenario, you'd want to use a Generative AI model to generate the solution.
-    return None
+    ########################
+    def default():
+        return []
+    ########################
+    def prime(n):
+        if n <= 1:
+            return [False]
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return [False]
+        return [True]
+    #######################
+    def fibonacci(n):
+        if n == 0:
+            return [0]
+        elif n == 1:
+            return [1]
+        else:
+            return [fibonacci(n - 1)[0] + fibonacci(n - 2)[0]]
+    ######################
+    def sum(a: int, b: int) -> int:
+        """
+        Calculate the sum of two numbers.
+
+        Parameters:
+        - a (int): First number
+        - b (int): Second number
+
+        Returns:
+        - int: Sum of a and b
+        """
+        result = a + b
+        return [result]
+    #######################
+    solution = default
+    # solution = fibonacci
+    # desc = problem["output_parameters"]
+    if ("sum" in problem["problem_statement"]):
+        # print("prime problem")
+        solution = sum
+    if ("prime" in problem["problem_statement"]):
+        # print("prime problem")
+        solution = prime
+    if ("Fibonacci" in problem["problem_statement"]):
+        solution = fibonacci
+        # print("fibonacci")
+    return solution
+
 
 def evaluate_solution(problem, solution):
     # This is a basic evaluator. It assumes the AI's solution is a function that can be executed.
     # It will run the function with sample inputs and compare the result with expected outputs.
     passed_tests = 0
     total_tests = len(problem["sample_test_cases"])
-    
+
     for test_case in problem["sample_test_cases"]:
         try:
             result = solution(*test_case["input"])
+            # print("input")
+            # print(test_case["input"])
+            # print("result")
+            # print(result)
+            # print("expected_output")
+            # print(test_case["expected_output"])
+            # print(total_tests)
             if result == test_case["expected_output"]:
                 passed_tests += 1
+
         except Exception as e:
             # If there's an error in the solution, it fails the test case
             pass
-            
+
     return passed_tests, total_tests
+
 
 def main():
     problems = load_problems("problems.json")
     results = []
-    
+    # """
     for problem in problems:
-        solution = generate_solution(problem)
-        passed_tests, total_tests = evaluate_solution(problem, solution)
-        results.append({
+        if(problem["problem_id"]) != "001": break
+        else:
+            solution = generate_solution(problem)
+            passed_tests, total_tests = evaluate_solution(problem, solution)
+            results.append({
             "problem_id": problem["problem_id"],
             "passed_tests": passed_tests,
             "total_tests": total_tests
-        })
-        
+            }
+            )
+
+
     # Print the results
     for result in results:
         print(f"Problem ID: {result['problem_id']}, Passed Tests: {result['passed_tests']}/{result['total_tests']}")
 
+
+# """
 if __name__ == "__main__":
     main()
